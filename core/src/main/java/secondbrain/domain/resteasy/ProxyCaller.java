bbbe8fb7b9d66a3b9d28ccc29ca8614bdb5f2c2e
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-07 18:01:34 +1000

    Stick with Vavr

--- core/src/main/java/secondbrain/domain/resteasy/ProxyCaller.java
+++ core/src/main/java/secondbrain/domain/resteasy/ProxyCaller.java
@@ -1,5 +1,6 @@
 package secondbrain.domain.resteasy;
 
+import io.vavr.control.Try;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.ClientErrorException;
@@ -10,13 +11,11 @@ import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 @ApplicationScoped
 public class ProxyCaller {
      public <T, U> U callProxy(@NotNull final String uri, @NotNull Class<T> proxyInterface, @NotNull ProxyCallback<T, U> callback) {
-        try (ClosableResteasyClient client = new ClosableResteasyClient((ResteasyClient) ClientBuilder.newClient())) {
-            final ResteasyWebTarget target = client.target(uri);
-            return callback.callProxy(target.proxy(proxyInterface));
-        } catch (final ClientErrorException ex) {
-            System.out.println(ex.getMessage());
-            throw ex;
-        }
+         return Try.withResources(() -> new ClosableResteasyClient((ResteasyClient) ClientBuilder.newClient()))
+                 .of(client -> client.target(uri))
+                 .map(target -> callback.callProxy(target.proxy(proxyInterface)))
+                 .onFailure(ClientErrorException.class, ex -> System.out.println(ex.getMessage()))
+                 .get();
     }
 }
 
