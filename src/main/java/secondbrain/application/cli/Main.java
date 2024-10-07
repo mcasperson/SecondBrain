96eb4c235d862a168c39bbaf1bfe5f777b4f4366
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-07 11:19:43 +1000

    Fixed up logging

--- src/main/java/secondbrain/application/cli/Main.java
+++ src/main/java/secondbrain/application/cli/Main.java
@@ -3,9 +3,11 @@ package secondbrain.application.cli;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 import secondbrain.domain.handler.PromptHandler;
+import secondbrain.domain.logging.LogConfig;
 
 public class Main {
     public static void main(final String[] args) {
+        LogConfig.init();
         final Weld weld = new Weld();
         try (WeldContainer weldContainer = weld.initialize()) {
             final String response = weldContainer.select(PromptHandler.class).get()
---
+++ src/main/java/secondbrain/domain/logging/LogConfig.java
@@ -0,0 +1,21 @@
+package secondbrain.domain.logging;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Startup;
+import jakarta.enterprise.event.Observes;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class LogConfig {
+    public static void init() {
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(Level.WARNING);
+        for (Handler h : rootLogger.getHandlers()) {
+            h.setLevel(Level.WARNING);
+        }
+    }
+}
