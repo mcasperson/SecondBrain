c5a5cfd3a0c6dbc480c7edfbe11add44582cd7d7
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-08 06:42:58 +1000

    Added some checks of the ollama responses

--- core/src/main/java/secondbrain/infrastructure/ollama/OllamaClient.java
+++ core/src/main/java/secondbrain/infrastructure/ollama/OllamaClient.java
@@ -1,6 +1,7 @@
 package secondbrain.infrastructure.ollama;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
@@ -8,12 +9,21 @@ import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class OllamaClient {
-    public OllamaResponse getTools(Client client, OllamaGenerateBody body) {
+    public OllamaResponse getTools(@NotNull final Client client, @NotNull final OllamaGenerateBody body) {
         try (final Response response = client.target("http://localhost:11434/api/generate")
                 .request()
                 .header("Content-Type", "application/json")
                 .header("Accept", "application/json")
                 .post(Entity.entity(body, MediaType.APPLICATION_JSON))) {
+
+            if (response.getStatus() == 404) {
+                throw new RuntimeException("Failed to call Ollama:\n" + response.getStatus() + "\nMake sure to run 'ollama pull llama3.2'");
+            }
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed to call Ollama:\n" + response.getStatus());
+            }
+
             return response.readEntity(OllamaResponse.class);
         }
     }
--- core/src/main/java/secondbrain/infrastructure/ollama/OllamaResponse.java
+++ core/src/main/java/secondbrain/infrastructure/ollama/OllamaResponse.java
@@ -1,7 +1,10 @@
 package secondbrain.infrastructure.ollama;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record OllamaResponse(String model,
                              String created_at,
                              String done,
