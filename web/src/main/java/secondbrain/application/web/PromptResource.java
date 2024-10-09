0d77d7d520dd634dc3ffdcc30cb8b3083295626d
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-09 15:02:19 +1000

    Fixed up http posts

--- web/src/main/java/secondbrain/application/web/PromptResource.java
+++ web/src/main/java/secondbrain/application/web/PromptResource.java
@@ -3,16 +3,18 @@ package secondbrain.application.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Preconditions;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Cookie;
-import jakarta.ws.rs.core.MediaType;
 import secondbrain.domain.handler.PromptHandler;
 import secondbrain.domain.json.JsonDeserializer;
 
 import java.util.Map;
 import java.util.Objects;
 
-
+@Path("promptweb")
 public class PromptResource {
     @Inject
     private PromptHandler promptHandler;
@@ -21,7 +23,6 @@ public class PromptResource {
     private JsonDeserializer jsonDeserializer;
 
     @POST
-    @Path("/promptweb")
     public String getWeb(@QueryParam("prompt") final String prompt,
                          @CookieParam("session") final Cookie session) throws JsonProcessingException {
 
@@ -31,18 +32,6 @@ public class PromptResource {
         final Map<String, String> context = session == null
                 ? Map.of()
                 : jsonDeserializer.deserializeMap(session.getValue(), String.class, String.class);
-
-        return promptHandler.handlePrompt(context, Objects.requireNonNullElse(prompt, ""));
-    }
-
-    @POST
-    @Path("/promptcli")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String getCli(@QueryParam("prompt") final String prompt,
-                         final Map<String, String> context) {
-
-        Preconditions.checkNotNull(promptHandler, "promptHandler must not be null");
-        Preconditions.checkNotNull(jsonDeserializer, "jsonDeserializer must not be null");
 
         return promptHandler.handlePrompt(context, Objects.requireNonNullElse(prompt, ""));
     }
