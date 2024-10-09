e6db7fa35093684bfc1d34fb763de5e79c938195
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-09 14:14:14 +1000

    Working on complete loop with Slack prompts

--- tools/src/main/java/secondbrain/tools/SlackChannel.java
+++ tools/src/main/java/secondbrain/tools/SlackChannel.java
@@ -1,16 +1,37 @@
 package secondbrain.tools;
 
+import com.slack.api.Slack;
+import com.slack.api.methods.response.conversations.ConversationsHistoryResponse;
+import com.slack.api.model.Conversation;
+import com.slack.api.model.Message;
+import io.vavr.control.Try;
 import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
 import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.client.ClientBuilder;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.jspecify.annotations.NonNull;
+import secondbrain.domain.args.ArgsAccessor;
+import secondbrain.domain.constants.Constants;
 import secondbrain.domain.tooldefs.Tool;
 import secondbrain.domain.tooldefs.ToolArgs;
 import secondbrain.domain.tooldefs.ToolArguments;
+import secondbrain.infrastructure.ollama.OllamaClient;
+import secondbrain.infrastructure.ollama.OllamaGenerateBody;
+import secondbrain.infrastructure.ollama.OllamaResponse;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Dependent
 public class SlackChannel implements Tool {
+    @Inject
+    private ArgsAccessor argsAccessor;
+
+    @Inject
+    private OllamaClient ollamaClient;
+
     @Override
     @NotNull
     public String getName() {
@@ -38,13 +59,67 @@ public class SlackChannel implements Tool {
             @NotNull final Map<String, String> context,
             @NotNull final String prompt,
             @NotNull final List<ToolArgs> arguments) {
-        if (arguments.size() != 2) {
-            return "Hi from Slack!";
+        final String channel = argsAccessor.getArgument(arguments, "channel", "");
+
+        // you can get this instance via ctx.client() in a Bolt app
+        var client = Slack.getInstance().methods();
+
+        final Try<String> id = Try.of(() -> client.conversationsList(r -> r.token(System.getenv("SLACK_BOT_TOKEN"))).getChannels())
+                .map(channels -> channels.stream().filter(c -> c.getName().equals(channel)).map(Conversation::getId).findFirst())
+                .map(Optional::get)
+                .onFailure(error -> System.out.println("Error: " + error))
+                .recover(e -> null);
+
+        if (id.isFailure()) {
+            return "Channel " + channel + " not found";
         }
 
-        return "Hi from Slack channel " + arguments.stream()
-                .filter(arg -> arg.argName().equals("channel"))
-                .findFirst().orElse(new ToolArgs("unknown", "unknown"))
-                .argValue();
+        final BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword(System.getenv("ENCRYPTION_PASSWORD"));
+        final String accessToken = textEncryptor.decrypt(context.get("access_token"));
+
+        final Try<String> messages = id
+                .mapTry(chanId -> client.conversationsHistory(r -> r.token(accessToken).channel(chanId)))
+                .map(this::conversationsToText);
+
+        if (messages.isFailure()) {
+            return "Messages could not be read";
+        }
+
+        final String messageContext = buildToolPrompt(messages.get(), prompt);
+
+        return Try.of(() -> callOllama(messageContext))
+                .map(OllamaResponse::response)
+                .recover(throwable -> "Failed to call Ollama: " + throwable.getMessage())
+                .get();
+
+    }
+
+    @NotNull
+    public String buildToolPrompt(@NotNull final String context, @NonNull final String prompt) {
+        return """
+                <|begin_of_text|>
+                <|start_header_id|>system<|end_header_id|>
+                You are professional agent that understands Slack conversations. 
+                You are given the history of a Slack channel and asked to answer questions based on the messages provided.
+                Here are the messages:
+                """
+                + context.substring(0, Math.min(context.length(), Constants.MAX_CONTEXT_LENGTH))
+                + "<|eot_id|><|start_header_id|>user<|end_header_id|>"
+                + prompt
+                + "<|eot_id|><|start_header_id|>assistant<|end_header_id|>".stripLeading();
+    }
+
+    private String conversationsToText(@NotNull final ConversationsHistoryResponse conversation) {
+        return conversation.getMessages()
+                .stream()
+                .map(Message::getText)
+                .reduce("", (a, b) -> a + "\n" + b);
+    }
+
+    private OllamaResponse callOllama(@NotNull final String llmPrompt) {
+        return ollamaClient.getTools(
+                ClientBuilder.newClient(),
+                new OllamaGenerateBody("llama3.2", llmPrompt, false));
     }
 }
--- web/src/main/java/secondbrain/application/web/PromptResource.java
+++ web/src/main/java/secondbrain/application/web/PromptResource.java
@@ -3,10 +3,11 @@ package secondbrain.application.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Preconditions;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.CookieParam;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Cookie;
 import secondbrain.domain.handler.PromptHandler;
 import secondbrain.domain.json.JsonDeserializer;
 
@@ -23,13 +24,15 @@ public class PromptResource {
 
     @GET
     public String get(@QueryParam("prompt") final String prompt,
-                      @HeaderParam("context") final String context) throws JsonProcessingException {
+                      @CookieParam("session") final Cookie session) throws JsonProcessingException {
 
         Preconditions.checkNotNull(promptHandler, "promptHandler must not be null");
         Preconditions.checkNotNull(jsonDeserializer, "jsonDeserializer must not be null");
 
-        final Map<String, String> contextMap = jsonDeserializer.deserializeMap(
-                Objects.requireNonNullElse(context, "{}"), String.class, String.class);
-        return promptHandler.handlePrompt(contextMap, Objects.requireNonNullElse(prompt, ""));
+        final Map<String, String> context = session == null
+                ? Map.of()
+                : jsonDeserializer.deserializeMap(session.getValue(), String.class, String.class);
+
+        return promptHandler.handlePrompt(context, Objects.requireNonNullElse(prompt, ""));
     }
 }
