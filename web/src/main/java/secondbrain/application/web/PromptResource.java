84a3682fbca206664ff8883f85d192b894977e56
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-11 07:53:57 +1000

    All the slack token to be supplied directly

--- web/src/main/java/secondbrain/application/web/PromptResource.java
+++ web/src/main/java/secondbrain/application/web/PromptResource.java
@@ -3,11 +3,9 @@ package secondbrain.application.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Preconditions;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.CookieParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Cookie;
+import jakarta.ws.rs.core.MediaType;
 import secondbrain.domain.handler.PromptHandler;
 import secondbrain.domain.json.JsonDeserializer;
 
@@ -23,16 +21,21 @@ public class PromptResource {
     private JsonDeserializer jsonDeserializer;
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public String getWeb(@QueryParam("prompt") final String prompt,
-                         @CookieParam("session") final Cookie session) throws JsonProcessingException {
+                         @CookieParam("session") final Cookie session,
+                         final Map<String, String> context) throws JsonProcessingException {
 
         Preconditions.checkNotNull(promptHandler, "promptHandler must not be null");
         Preconditions.checkNotNull(jsonDeserializer, "jsonDeserializer must not be null");
 
-        final Map<String, String> context = session == null
+        final Map<String, String> cookieContext = session == null
                 ? Map.of()
                 : jsonDeserializer.deserializeMap(session.getValue(), String.class, String.class);
 
-        return promptHandler.handlePrompt(context, Objects.requireNonNullElse(prompt, ""));
+        cookieContext.putAll(Objects.requireNonNullElse(context, Map.of()));
+
+        return promptHandler.handlePrompt(cookieContext, Objects.requireNonNullElse(prompt, ""));
     }
 }
