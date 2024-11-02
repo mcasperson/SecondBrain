478695c211d30a2cb1bd7173016647639485ab0c
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-02 14:46:06 +1100

    Wrap encoding argument

--- scripts/SendToSlack.ps1
+++ scripts/SendToSlack.ps1
@@ -74,7 +74,7 @@ Function Invoke-CustomCommand
 $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 
 # Replace the location of the Jar file with your copy of the CLI UberJAR
-$result = Invoke-CustomCommand java "-Dstdout.encoding=UTF-8 -jar C:\Apps\secondbrain-cli-1.0-SNAPSHOT.jar `"Summarize 7 days worth of messages from the '$( $args[0] )' Slack channel in the style of a news article with up to 3 paragraphs. You can use fewer paragraphs if there is only a small amount of chat text to summarize. Use plain language. You will be penalized for using emotive or excited language.`" markdn"
+$result = Invoke-CustomCommand java "`"-Dstdout.encoding=UTF-8`" -jar C:\Apps\secondbrain-cli-1.0-SNAPSHOT.jar `"Summarize 7 days worth of messages from the '$( $args[0] )' Slack channel in the style of a news article with up to 3 paragraphs. You can use fewer paragraphs if there is only a small amount of chat text to summarize. Use plain language. You will be penalized for using emotive or excited language.`" markdn"
 
 echo $result.StdOut
 echo $result.StdErr
