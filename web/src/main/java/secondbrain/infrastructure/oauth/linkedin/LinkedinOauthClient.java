60ea3162bd7eaf4d15a9536a15ff783ca95a5a30
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-05 16:12:51 +1000

    Fixed LinkedIn token exchange

--- web/src/main/java/secondbrain/infrastructure/oauth/linkedin/LinkedinOauthClient.java
+++ web/src/main/java/secondbrain/infrastructure/oauth/linkedin/LinkedinOauthClient.java
@@ -1,33 +1,30 @@
 package secondbrain.infrastructure.oauth.linkedin;
 
-import com.google.common.collect.ImmutableList;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.EntityPart;
-import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 
 import java.io.IOException;
-import java.util.List;
 
 @ApplicationScoped
 public class LinkedinOauthClient {
     public LinkedInOauthTokenResponse exchangeToken(final Client client, final String redirectUri, final String code, final String clientId, final String clientSecret) throws IOException {
 
-        final List<EntityPart> multipart = ImmutableList.of(
-                EntityPart.withName("grant_type").content("authorization_code").build(),
-                EntityPart.withName("redirect_uri").content(redirectUri).build(),
-                EntityPart.withName("code").content(code).build(),
-                EntityPart.withName("client_id").content(clientId).build(),
-                EntityPart.withName("client_secret").content(clientSecret).build());
+        final MultivaluedMap<String, String> body = new MultivaluedHashMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
 
         try (final Response response = client.target("https://www.linkedin.com/oauth/v2/accessToken")
-                .request(MediaType.MULTIPART_FORM_DATA_TYPE)
+                .request(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
                 .accept(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.entity(new GenericEntity<>(multipart) {
-                }, MediaType.MULTIPART_FORM_DATA))) {
+                .post(Entity.entity(body, MediaType.APPLICATION_FORM_URLENCODED))) {
             return response.readEntity(LinkedInOauthTokenResponse.class);
         }
     }
