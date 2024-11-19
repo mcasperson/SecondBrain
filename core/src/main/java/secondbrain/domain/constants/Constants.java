f6ad4782e5ffd20eff114acdfe54eb3cce43319f
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-19 11:42:18 +1000

    Increase the context length - llama 3.2 supports 128k

--- core/src/main/java/secondbrain/domain/constants/Constants.java
+++ core/src/main/java/secondbrain/domain/constants/Constants.java
@@ -3,8 +3,8 @@ package secondbrain.domain.constants;
 public class Constants {
     /**
      * The maximum length of a context that can be passed to the Llama3.2 LLM hosted
-     * by Ollama. The context window is 8K, with an average of 4 characters per
-     * token. We have a buffer for the prompt, so we can use 7000 tokens.
+     * by Ollama. The context window is 128K, with an average of 4 characters per
+     * token.
      */
-    public static final int MAX_CONTEXT_LENGTH = 7000 * 4;
+    public static final int MAX_CONTEXT_LENGTH = 100000 * 4;
 }
