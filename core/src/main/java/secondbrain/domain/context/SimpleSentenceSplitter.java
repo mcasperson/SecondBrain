5b4b57033f36ce837d0638db0a5c4ada88859f1b
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-19 14:48:11 +1000

    Filter out small sentences

--- core/src/main/java/secondbrain/domain/context/SimpleSentenceSplitter.java
+++ core/src/main/java/secondbrain/domain/context/SimpleSentenceSplitter.java
@@ -18,6 +18,7 @@ public class SimpleSentenceSplitter implements SentenceSplitter {
 
         return Stream.of(document.split("\\r\\n|\\r|\\n|\\.|;|!|\\?"))
                 .filter(sentence -> !sentence.isBlank())
+                .filter(sentence -> sentence.split("\\s+").length >= 5)
                 .map(String::trim)
                 // Remove list formatting
                 .map(sentence -> sentence.replaceFirst("^\\* ", ""))
