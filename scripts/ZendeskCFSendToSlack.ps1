c72cca4083c9aa74e2d972a8fab14070e798c18c
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-24 13:19:33 +1000

    Add a cf zendesk script

---
+++ scripts/ZendeskCFSendToSlack.ps1
@@ -0,0 +1,127 @@
+$global:stdErr = [System.Text.StringBuilder]::new()
+$global:myprocessrunning = $true
+
+Function Invoke-CustomCommand
+{
+    Param (
+        $commandPath,
+        $commandArguments,
+        $workingDir = (Get-Location),
+        $path = @()
+    )
+
+    $global:stdErr.Clear()
+    $global:myprocessrunning = $true
+
+    $path += $env:PATH
+    $newPath = $path -join [IO.Path]::PathSeparator
+
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $commandPath
+    $pinfo.WorkingDirectory = $workingDir
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $pinfo.StandardErrorEncoding = [System.Text.Encoding]::UTF8
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $commandArguments
+    $pinfo.EnvironmentVariables["PATH"] = $newPath
+    $p = New-Object System.Diagnostics.Process
+
+    # https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.process.standardoutput
+    # Reading from one stream must be async
+    # We read the error stream, because events can be handled out of order,
+    # and it is better to have this happen with debug output
+    Register-ObjectEvent -InputObject $p -EventName "ErrorDataReceived" -Action {
+        $global:stdErr.AppendLine($EventArgs.Data)
+    } | Out-Null
+
+    # We must wait for the Exited event rather than WaitForExit()
+    # because WaitForExit() can result in events being missed
+    # https://stackoverflow.com/questions/13113624/captured-output-of-command-run-by-powershell-is-sometimes-incomplete
+    Register-ObjectEvent -InputObject $p -EventName "Exited" -action {
+        $global:myprocessrunning = $false
+    } | Out-Null
+
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+
+    $p.BeginErrorReadLine()
+
+    # Wait 30 minutes before forcibly killing the process
+    $processTimeout = 1000 * 60 * 30
+    while (($global:myprocessrunning -eq $true) -and ($processTimeout -gt 0))
+    {
+        # We must use lots of shorts sleeps rather than a single long one otherwise events are not processed
+        $processTimeout -= 50
+        Start-Sleep -m 50
+    }
+
+    $output = $p.StandardOutput.ReadToEnd()
+
+    if ($processTimeout -le 0)
+    {
+        $p.Kill()
+    }
+
+    $executionResults = [pscustomobject]@{
+        StdOut = $output
+        StdErr = $global:stdErr.ToString()
+        ExitCode = $p.ExitCode
+    }
+
+    return $executionResults
+}
+
+Function Get-FullException
+{
+    Param ($error)
+
+    $e = $error.Exception
+
+    $msg = $e.Message
+    while ($e.InnerException)
+    {
+        $e = $e.InnerException
+        $msg += "`n" + $e.Message
+    }
+    return $msg
+}
+
+# Powershell has to be set to parse the output of an executable as UTF8
+# Java will print to std out as UTF 8 by passing -Dstdout.encoding=UTF-8
+$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+$jarFile = "C:\Apps\secondbrain-cli-1.0-SNAPSHOT.jar"
+#$jarFile = "../cli/target/secondbrain-cli-1.0-SNAPSHOT.jar"
+
+
+$ticketResult = Invoke-CustomCommand java "`"-Dsb.zendesk.user=$( $env:SB_ZENDESK_USER_CF )`" `"-Dsb.zendesk.url=$( $env:SB_ZENDESK_URL_CF )`" `"-Dsb.zendesk.accesstoken=$( $env:SB_ZENDESK_ACCESSTOKEN_CF )`" `"-Dsb.ollama.toolmodel=llama3.2`" `"-Dsb.ollama.model=gemma2`" `"-Dsb.ollama.contextlength=32000`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"Given 8 hours worth of ZenDesk tickets, provide a summary of the questions and problems in the style of a news article with up to 3 paragraphs. You must carefully consider each ticket when genering the summary. You will be penalized for showing category percentages. You will be penalized for including ticket IDs or reference numbers. Use concise, plain, and professional language. You will be penalized for using emotive or excited language. You will be penalized for including a generic final summary paragraph. You must only summarize emails that are asking for support. You will be penalized for summarizing emails that are not asking for support. You will be penalized for summarizing marketing emails.`" markdn"
+
+echo "ZenDesk StdOut"
+echo $ticketResult.StdOut
+
+if ($ticketResult.StdOut -match "No tickets found")
+{
+    exit 0
+}
+
+# Replace this URL with your own Slack web hook
+$uriSlack = $env:SB_SLACK_ZENDESK_WEBHOOK_CF
+$body = ConvertTo-Json @{
+    type = "mrkdwn"
+    text = $ticketResult.StdOut
+}
+
+try
+{
+    Invoke-RestMethod -uri $uriSlack -Method Post -body $body -ContentType 'application/json' | Out-Null
+}
+catch
+{
+    Write-Error "$( Get-Date ) : Update to Slack went wrong..."
+    Write-Error (Get-FullException $_)
+}
+
+
+
