ff4b6c4cd47e1ff8cd0e8b6f9631159623cc591f
Matthew Casperson <matthewcasperson@gmail.com>
2024-12-02 07:53:37 +1000

    Fixed up access to encryption password

--- core/src/main/java/secondbrain/domain/encryption/JasyptEncryptor.java
+++ core/src/main/java/secondbrain/domain/encryption/JasyptEncryptor.java
@@ -1,5 +1,6 @@
 package secondbrain.domain.encryption;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -7,13 +8,14 @@ import org.jasypt.util.text.BasicTextEncryptor;
 
 @ApplicationScoped
 public class JasyptEncryptor implements Encryptor {
-    private final BasicTextEncryptor textEncryptor;
+    private final BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+    
     @Inject
-    @ConfigProperty(name = "sb.encryption.password", defaultValue = "12345678")
+    @ConfigProperty(name = "sb.encryption.password", defaultValue = "123456789")
     String encryptionPassword;
 
-    public JasyptEncryptor() {
-        textEncryptor = new BasicTextEncryptor();
+    @PostConstruct
+    public void construct() {
         textEncryptor.setPassword(encryptionPassword);
     }
 
