86df0e31661ae39f16a49260f45067bbd288c1bb
Matthew Casperson <matthewcasperson@gmail.com>
2024-12-02 12:59:32 +1000

    Model needs to be trimmed

--- core/src/main/java/secondbrain/infrastructure/ollama/OllamaGenerateBody.java
+++ core/src/main/java/secondbrain/infrastructure/ollama/OllamaGenerateBody.java
@@ -1,7 +1,7 @@
 package secondbrain.infrastructure.ollama;
 
 public record OllamaGenerateBody(String model, String prompt, Boolean stream) {
-    public OllamaGenerateBody getSanitizedBody() {
+    public OllamaGenerateBody sanitizedCopy() {
         return new OllamaGenerateBody(model.trim(), prompt.trim(), stream);
     }
 }
