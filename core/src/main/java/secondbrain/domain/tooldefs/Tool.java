57fc2d4827f5776212171dc1dc54036ce30852fe
Matthew Casperson <matthewcasperson@gmail.com>
2024-12-12 18:32:18 +1000

    Added more comments

--- core/src/main/java/secondbrain/domain/tooldefs/Tool.java
+++ core/src/main/java/secondbrain/domain/tooldefs/Tool.java
@@ -36,6 +36,14 @@ public interface Tool<T> {
      * Builds the initial context for the tool. This context is either the raw data from the upstream source
      * (think Slack messages etc.) or the processed version of the data (think git commits that have been
      * summarized into a paragraph).
+     * <p>
+     * This context is either used to build the output of this tool, or a meta-tool combines this context with the
+     * context of other tools to answer questions that span multiple data sources.
+     * <p>
+     * The difference between getContext() and call() is that getContext() provides the information required to answer
+     * a question, while call() actually answers the question. While getContext() may call the LLM to produce a summary
+     * of the upstream data (perhaps to condense large amounts of upstream data or convert data like git diffs into plain
+     * text), getContext() does not typically pass the prompt argument to the LLM.
      *
      * @param context   The context associated with the prompt. These are values that come from the environment (like credentials) rather than from the prompt.
      * @param prompt    The prompt.
