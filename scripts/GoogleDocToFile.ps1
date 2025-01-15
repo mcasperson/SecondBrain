6ab1c97907e2312ff47babc398dc4f9e65591b11
Matthew Casperson <matthew.casperson@octopus.com>
2025-01-15 20:29:49 +1000

    Added more logging

--- scripts/GoogleDocToFile.ps1
+++ scripts/GoogleDocToFile.ps1
@@ -126,6 +126,8 @@ $companyNames -split "," | ForEach-Object {
         $keywords = $split[1..($array.Length - 1)] -join ","
     }
 
+    Write-Host "Processing $companyName with keywords $keywords"
+
     $ticketResult = Invoke-CustomCommand java "`"-Dsb.ollama.contextwindow=8192`" `"-Dsb.ollama.diffcontextwindow=8192`" `"-Dsb.zendesk.excludedorgs=$( $env:EXCLUDED_ORGANIZATIONS )`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"You are given the Google document with the id $googleDoc. Assume the document is written in the first person by Matthew Casperson, also known as Matt. Trim the document with keywords '$keywords'. List all the information about $companyName. You will be penalize for including details about unrelated companies.`""
 
     Set-Content -Path "$obsidianPath\$companyName.md" -Value $ticketResult.StdOut
