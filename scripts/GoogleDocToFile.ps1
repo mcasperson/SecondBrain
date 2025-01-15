6d58ef919fe925e33feffba257eeb6f0675df23e
Matthew Casperson <matthew.casperson@octopus.com>
2025-01-15 20:46:50 +1000

    Use llama3.1

--- scripts/GoogleDocToFile.ps1
+++ scripts/GoogleDocToFile.ps1
@@ -109,7 +109,7 @@ $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 #$jarFile = "C:\Apps\secondbrain-cli-1.0-SNAPSHOT.jar"
 $jarFile = "/home/matthew/Code/SecondBrain/cli/target/secondbrain-cli-1.0-SNAPSHOT.jar"
 
-$model = "llama3.3"
+$model = "llama3.1"
 $toolModel = "llama3.1"
 
 $companyNames -split "," | ForEach-Object {
