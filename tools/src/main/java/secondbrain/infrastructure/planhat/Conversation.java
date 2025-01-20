ceffc0971d97fb2186dba4c5a0318fcf3431cd98
Matthew Casperson <matthew.casperson@octopus.com>
2025-01-21 06:40:46 +1000

    Capture the subject

--- tools/src/main/java/secondbrain/infrastructure/planhat/Conversation.java
+++ tools/src/main/java/secondbrain/infrastructure/planhat/Conversation.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Conversation(@JsonProperty("_id") String id, String description, String snippet, String date, String companyId) {
+public record Conversation(@JsonProperty("_id") String id, String description, String snippet, String date, String companyId, String subject) {
     public Conversation updateDescriptionAndSnippet(String description, String snippet) {
-        return new Conversation(id, description, snippet, date, companyId);
+        return new Conversation(id, description, snippet, date, companyId, subject);
     }
 
     public String getContent() {
