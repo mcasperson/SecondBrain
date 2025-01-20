61e3e229b1096f8d8a10dd4bc1816ed43e385a31
Matthew Casperson <matthew.casperson@octopus.com>
2025-01-21 08:43:27 +1000

    Basic PDF generation is working

--- scripts/publish/create_pdf.py
+++ scripts/publish/create_pdf.py
@@ -19,13 +19,15 @@ class PDF(FPDF):
 
     def chapter_body(self, body):
         self.set_font('DejaVu', '', 12)
-        self.multi_cell(0, 10, body)
+        self.write_html(body)
         self.ln()
 
 def convert_md_to_pdf(directory, output_pdf):
     pdf = PDF()
+    pdf.add_font('DejaVu', '', '/home/matthew/Code/SecondBrain/scripts/publish/fonts/DejaVuSansCondensed.ttf')
+    pdf.add_font('DejaVu', 'B', '/home/matthew/Code/SecondBrain/scripts/publish/fonts/DejaVuSansCondensed-Bold.ttf')
+    pdf.add_font('DejaVu', 'I', '/home/matthew/Code/SecondBrain/scripts/publish/fonts/DejaVuSansCondensed-Oblique.ttf')
     pdf.add_page()
-    pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
 
     for filename in os.listdir(directory):
         if filename.endswith(".md"):
@@ -40,5 +42,5 @@ def convert_md_to_pdf(directory, output_pdf):
 
 if __name__ == "__main__":
     directory = "/home/matthew/.config/JetBrains/IntelliJIdea2024.3/scratches"
-    output_pdf = "/home/matthew/Desktop/output.pdf"
+    output_pdf = "/home/matthew/Dropbox/output.pdf"
     convert_md_to_pdf(directory, output_pdf)
