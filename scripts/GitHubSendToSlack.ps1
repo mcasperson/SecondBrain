d4b060a04e7a55b6cbe6812bcfc803ba2c668c41
Matthew Casperson <matthew.casperson@octopus.com>
2025-01-21 06:30:06 +1000

    Use the larger model for git summaries, and include a larger context window

--- scripts/GitHubSendToSlack.ps1
+++ scripts/GitHubSendToSlack.ps1
@@ -110,11 +110,11 @@ $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 $jarFile = "/home/matthew/Code/SecondBrain/cli/target/secondbrain-cli-1.0-SNAPSHOT.jar"
 
 $model = "mistral-nemo"
-$gitDiffModel = "qwen2.5-coder:7b"
+$gitDiffModel = "qwen2.5-coder:14b"
 $toolModel = "llama3.1"
-$contextLength = "4096"
+$contextLength = "16384"
 
-$ticketResult = Invoke-CustomCommand java "`"-Dsb.tools.force=GitHubDiffs`" `"-Dsb.github.owner=$githubOwner`" `"-Dsb.github.repo=$githubRepo`" `"-Dsb.github.days=1`" `"-Dsb.github.branch=main`" `"-Dsb.ollama.gitdiffmodel=$gitDiffModel`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" `"-Dsb.ollama.contextwindow=$contextLength`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"Given the git diffs, provide a summary of the changes. Use plain language. You will be penalized for offering code suggestions. You will be penalized for sounding excited about the changes.`" markdn"
+$ticketResult = Invoke-CustomCommand java "`"-Dsb.tools.force=GitHubDiffs`" `"-Dsb.github.owner=$githubOwner`" `"-Dsb.github.repo=$githubRepo`" `"-Dsb.github.days=1`" `"-Dsb.github.branch=main`" `"-Dsb.ollama.gitdiffmodel=$gitDiffModel`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" `"-Dsb.ollama.contextwindow=$contextLength`" `"-Dsb.ollama.diffcontextwindow=$contextLength`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"Given the git diffs, provide a summary of the changes. Use plain language. You will be penalized for offering code suggestions. You will be penalized for sounding excited about the changes.`" markdn"
 
 echo "ZenDesk StdOut for $githubOwner and $githubRepo"
 echo $ticketResult.StdOut
