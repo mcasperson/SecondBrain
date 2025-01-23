0d4300c46125bb2d92500399bdfb49c9ad7eb2b8
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-23 17:28:39 +1000

    Fixing the prompt

--- scripts/OrganizationMultiGeneratePdf.ps1
+++ scripts/OrganizationMultiGeneratePdf.ps1
@@ -168,7 +168,7 @@ foreach ($entity in $database.entities)
     Start-Sleep -Seconds 60
 }
 
-$result = Invoke-CustomCommand java "`"-Dstdout.encoding=UTF-8`" `"-Dsb.tools.force=DirectoryScan`" `"-Dsb.directoryscan.individualdocumentprompt=Summarize the document as a single paragraph`" `"-Dsb.directoryscan.exclude=Executive Summary.md`" `"-Dsb.ollama.contextwindow=$contextWindow`" `"-Dsb.exceptions.printstacktrace=true`" `"-Dsb.directoryscan.directory=$subDir`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" -jar $jarFile `"Summarize each of the supplied File Contents into a combined executive summary.`""
+$result = Invoke-CustomCommand java "`"-Dstdout.encoding=UTF-8`" `"-Dsb.tools.force=DirectoryScan`" `"-Dsb.directoryscan.individualdocumentprompt=Summarize the document as a single paragraph`" `"-Dsb.directoryscan.exclude=Executive Summary.md`" `"-Dsb.ollama.contextwindow=$contextWindow`" `"-Dsb.exceptions.printstacktrace=true`" `"-Dsb.directoryscan.directory=$subDir`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" -jar $jarFile `"Summarize each of the supplied File Contents under a header including the company name and a paragraph with a summary of the company's activities.`""
 Set-Content -Path "$subDir/Executive Summary.md"  -Value $result.StdOut
 Add-Content -Path /tmp/pdfgenerate.log -Value $result.StdOut
 
