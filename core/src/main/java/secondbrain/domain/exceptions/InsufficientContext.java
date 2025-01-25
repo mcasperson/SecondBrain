d78636528a158ec34011228e6ad791780acbb7e3
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-26 07:30:11 +1000

    These fields were unnecessary

--- core/src/main/java/secondbrain/domain/exceptions/InsufficientContext.java
+++ core/src/main/java/secondbrain/domain/exceptions/InsufficientContext.java
@@ -1,17 +1,8 @@
 package secondbrain.domain.exceptions;
 
 public class InsufficientContext extends RuntimeException {
-    private String body;
-    private int code;
-
     public InsufficientContext() {
         super();
-    }
-
-    public InsufficientContext(final String message, final String body, final int code) {
-        super(message);
-        this.body = body;
-        this.code = code;
     }
 
     public InsufficientContext(final String message, final Throwable cause) {
@@ -24,13 +15,5 @@ public class InsufficientContext extends RuntimeException {
 
     public InsufficientContext(final Throwable cause) {
         super(cause);
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public int getCode() {
-        return code;
     }
 }
