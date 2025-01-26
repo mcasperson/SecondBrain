085c6cce7b8917a622cd14ab585023b0d2d2678e
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-26 18:41:29 +1000

    Added tests

--- core/src/test/java/secondbrain/domain/persist/H2LocalStorageTest.java
+++ core/src/test/java/secondbrain/domain/persist/H2LocalStorageTest.java
@@ -1,17 +1,59 @@
 package secondbrain.domain.persist;
 
+import io.smallrye.config.PropertiesConfigSource;
+import io.smallrye.config.SmallRyeConfigBuilder;
+import io.smallrye.config.inject.ConfigExtension;
 import io.vavr.control.Try;
-import org.junit.Assert;
-import org.junit.Test;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
+import org.jboss.weld.junit5.auto.AddBeanClasses;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import secondbrain.domain.json.JsonDeserializerJackson;
 
+import java.util.Map;
 import java.util.UUID;
 
+@EnableAutoWeld
+@AddExtensions(ConfigExtension.class)
+@AddBeanClasses(H2LocalStorage.class)
+@AddBeanClasses(JsonDeserializerJackson.class)
 public class H2LocalStorageTest {
+
+    @Inject
+    H2LocalStorage h2LocalStorage;
+
+    /**
+     * <a href="https://github.com/weld/weld-testing/issues/81#issuecomment-1564002983">...</a>
+     */
+    @BeforeEach
+    void updateConfig() {
+        final var configSource = new PropertiesConfigSource(
+                Map.of("sb.cache.disable", "false"),
+                "TestConfig",
+                Integer.MAX_VALUE
+        );
+        final Config newConfig = new SmallRyeConfigBuilder()
+                .withSources(configSource)
+                .build();
+
+        final var configProviderResolver = ConfigProviderResolver.instance();
+        final var oldConfig = configProviderResolver.getConfig();
+
+        configProviderResolver.releaseConfig(oldConfig);
+        configProviderResolver.registerConfig(
+                newConfig,
+                Thread.currentThread().getContextClassLoader()
+        );
+    }
 
     @Test
     public void testConnection() {
-        final H2LocalStorage h2LocalStorage = new H2LocalStorage();
-        Assert.assertNull(h2LocalStorage.getString(
+        Assertions.assertNull(h2LocalStorage.getString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 UUID.randomUUID().toString()));
@@ -19,37 +61,49 @@ public class H2LocalStorageTest {
 
     @Test
     public void testSave() {
-        final H2LocalStorage h2LocalStorage = new H2LocalStorage();
         final String randomValue = UUID.randomUUID().toString();
-        Assert.assertEquals(randomValue, h2LocalStorage.getOrPutString(
+        Assertions.assertEquals(randomValue, h2LocalStorage.getOrPutString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 randomValue,
                 () -> randomValue));
-        Assert.assertEquals(randomValue, h2LocalStorage.getString(
+        Assertions.assertEquals(randomValue, h2LocalStorage.getString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 randomValue));
     }
 
     @Test
-    public void testTTL() {
-        final H2LocalStorage h2LocalStorage = new H2LocalStorage();
+    public void testSaveObject() {
         final String randomValue = UUID.randomUUID().toString();
-        Assert.assertEquals(randomValue, h2LocalStorage.getOrPutString(
+        Assertions.assertEquals(randomValue, h2LocalStorage.getOrPutObject(
+                H2LocalStorageTest.class.getSimpleName(),
+                "test",
+                randomValue,
+                TestObject.class,
+                () -> new TestObject(randomValue)).value());
+    }
+
+    @Test
+    public void testTTL() {
+        final String randomValue = UUID.randomUUID().toString();
+        Assertions.assertEquals(randomValue, h2LocalStorage.getOrPutString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 randomValue,
                 10,
                 () -> randomValue));
-        Assert.assertEquals(randomValue, h2LocalStorage.getString(
+        Assertions.assertEquals(randomValue, h2LocalStorage.getString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 randomValue));
         Try.run(() -> Thread.sleep(15000));
-        Assert.assertNull(h2LocalStorage.getString(
+        Assertions.assertNull(h2LocalStorage.getString(
                 H2LocalStorageTest.class.getSimpleName(),
                 "test",
                 randomValue));
+    }
+
+    record TestObject(String value) {
     }
 }
