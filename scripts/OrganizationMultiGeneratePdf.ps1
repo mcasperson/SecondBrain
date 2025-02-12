d31d2abf0eb48f0adce846c42774e73fa5322be7
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-12 13:52:26 +1000

    Try gemma2

--- scripts/OrganizationMultiGeneratePdf.ps1
+++ scripts/OrganizationMultiGeneratePdf.ps1
@@ -164,7 +164,7 @@ mkdir $subDir
 Write-Host "Working in $subDir"
 
 $toolModel = "llama3.1"
-$model = "mistral-nemo:12b-instruct-2407-q8_0"
+$model = "gemma2:27b"
 
 # Consider using K/V cache quanisation to support larger context windows with the following env vars:
 # OLLAMA_KV_CACHE_TYPE="q8_0"
