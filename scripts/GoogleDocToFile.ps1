b3b92592809cafb905c9a0924bd00e1eec8a2dab
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-12 11:27:36 +1000

    Use llama 3.3 for google doc summary

--- scripts/GoogleDocToFile.ps1
+++ scripts/GoogleDocToFile.ps1
@@ -117,8 +117,9 @@ $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 #$jarFile = "C:\Apps\secondbrain-cli-1.0-SNAPSHOT.jar"
 $jarFile = "/home/matthew/Code/SecondBrain/cli/target/secondbrain-cli-1.0-SNAPSHOT.jar"
 
-$model = "llama3.1"
+$model = "llama3.3"
 $toolModel = "llama3.1"
+$contextWindow = "32768"
 
 $companyNames -split "," | ForEach-Object {
     $split = $_ -split ":"
@@ -128,7 +129,7 @@ $companyNames -split "," | ForEach-Object {
 
     Write-Host "Processing $companyName with keywords $keywords"
 
-    $ticketResult = Invoke-CustomCommand java "`"-Dsb.tools.force=GoogleDocs`" `"-Dsb.ollama.contextwindow=8192`" `"-Dsb.google.doc=$googleDoc`" `"-Dsb.google.keywords=$keywords`" `"-Dsb.zendesk.excludedorgs=$( $env:EXCLUDED_ORGANIZATIONS )`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"You are given the Google document. Assume the document is written in the first person by Matthew Casperson, also known as Matt. List all the information about $companyName. Include a list of people and their job titles, platforms, and tools. List the number of projects, targets, and tenants. You will be penalize for including details about unrelated companies.`""
+    $ticketResult = Invoke-CustomCommand java "`"-Dsb.tools.force=GoogleDocs`" `"-Dsb.ollama.contextwindow=$contextWindow`" `"-Dsb.ollama.contextwindow=8192`" `"-Dsb.google.doc=$googleDoc`" `"-Dsb.google.keywords=$keywords`" `"-Dsb.zendesk.excludedorgs=$( $env:EXCLUDED_ORGANIZATIONS )`" `"-Dsb.ollama.toolmodel=$toolModel`" `"-Dsb.ollama.model=$model`" `"-Dstdout.encoding=UTF-8`" -jar $jarFile `"You are given the Google document. Assume the document is written in the first person by Matthew Casperson, also known as Matt. List all the information about $companyName. Include a list of people and their job titles, platforms, and tools. List the number of projects, targets, and tenants. You will be penalize for including details about unrelated companies.`""
 
     Set-Content -Path "$obsidianPath\$companyName.md" -Value $ticketResult.StdOut
 }
