d150e9c0e341cb051eea28025c875cae1b1438b6
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-18 08:13:52 +1000

    Now fixing in jobs

--- scripts/OrganizationMultiGeneratePdf.ps1
+++ scripts/OrganizationMultiGeneratePdf.ps1
@@ -90,6 +90,8 @@ if ($GenerateCompanyReports)
 
             Import-Module $PSScriptRoot/SharedFunctions.psm1
 
+            Invoke-CustomCommand echo "hi"
+
             $entityName = ($using:entity).name
 
             $EntityLog = "/tmp/pdfgenerate $entityName $( Get-Date -Format "yyyy-MM-dd HH:mm:ss" ).log"
