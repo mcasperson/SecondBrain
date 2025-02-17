20b2f48065580183beeb15b873cf05b2e9cb1bd1
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-18 07:53:29 +1000

    Still trying to debug modules

--- scripts/SharedFunctions.psm1
+++ scripts/SharedFunctions.psm1
@@ -50,7 +50,7 @@ Function Invoke-CustomCommand
     # https://stackoverflow.com/questions/13113624/captured-output-of-command-run-by-powershell-is-sometimes-incomplete
     $exitedEvent = {
         Write-Host "Process exited" -ForegroundColor yellow
-        WriteHost $sharedState
+        Write-Host $sharedState
         $sharedState["myprocessrunning"] = $false
     }.GetNewClosure()
     Register-ObjectEvent -InputObject $p -EventName "Exited" -action $exitedEvent | Out-Null
