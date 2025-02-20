f920decc488549ebc8c91e4c5eaf411f4f5cf67f
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-21 07:57:05 +1000

    Blocked raw downloads behind a semaphore

--- core/src/main/java/secondbrain/domain/reader/UrlReadingStrategy.java
+++ core/src/main/java/secondbrain/domain/reader/UrlReadingStrategy.java
@@ -5,31 +5,38 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.core.Response;
+import secondbrain.domain.concurrency.SemaphoreLender;
+import secondbrain.domain.constants.Constants;
 import secondbrain.domain.exceptions.InvalidResponse;
 import secondbrain.domain.response.ResponseValidation;
 
 @ApplicationScoped
 public class UrlReadingStrategy implements FileReadingStrategy {
+    private static final SemaphoreLender SEMAPHORE_LENDER = new SemaphoreLender(Constants.DEFAULT_SEMAPHORE_COUNT);
+
     @Inject
     private ResponseValidation responseValidation;
 
     private String downloadDocument(final Client client, final String url) {
-        String newUrl = url;
-        int count = 0;
+        return Try.withResources(SEMAPHORE_LENDER::lend)
+                .of(s -> downloadDocumentLoop(client, url, 0))
+                .get();
+    }
 
-        do {
-            try (Response response = client.target(newUrl).request().get()) {
-                if (response.getStatus() == 302 || response.getStatus() == 301) {
-                    newUrl = response.getHeaderString("Location");
-                } else {
-                    return responseValidation.validate(response, newUrl).readEntity(String.class);
-                }
-            }
-            count++;
-        } while (count < 5);
+    private String downloadDocumentLoop(final Client client, final String url, int retry) {
+        if (retry > 5) {
+            throw new InvalidResponse("Failed to get document from " + url);
+        }
 
-        throw new InvalidResponse("Failed to get document from " + url);
+        return Try.withResources(() -> client.target(url).request().get())
+                .of(response -> {
+                    if (response.getStatus() == 302 || response.getStatus() == 301) {
+                        return downloadDocumentLoop(client, response.getHeaderString("Location"), retry + 1);
+                    } else {
+                        return responseValidation.validate(response, url).readEntity(String.class);
+                    }
+                })
+                .get();
     }
 
     @Override
