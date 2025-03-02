ac712edfb3098285bc9c4a19084d99400de06323
Matthew Casperson <matthewcasperson@gmail.com>
2025-03-02 18:33:22 +1000

    Load models from shared list

---
+++ web/src/main/webapp/resources/js/help.js
@@ -0,0 +1,12 @@
+function setModelHelp() {
+    const model = document.getElementById('customModel').value
+    document.getElementById('customModelHelp').innerHTML = `
+        These models must be
+         <a href="https://github.com/ollama/ollama?tab=readme-ov-file#pull-a-model">pulled by Ollama</a>
+         e.g. <code>ollama pull ` + model + `</code> (when running Ollama locally)
+         or <code>docker exec secondbrain-ollama-1 ollama pull ` + model + `</code> (when running Ollama in Docker).`;
+}
+
+document.getElementById('customModel').onchange = setModelHelp
+
+setModelHelp()
---
+++ web/src/main/webapp/resources/js/models.js
@@ -0,0 +1,29 @@
+function addOptionsToSelect(selectId, options) {
+    const selectElement = document.getElementById(selectId);
+    options.forEach(optionValue => {
+        const option = document.createElement('option');
+        option.value = optionValue;
+        option.text = optionValue;
+        selectElement.appendChild(option);
+    });
+}
+
+const options = [
+    'mistral-nemo',
+    'llama3.1',
+    'llama3.2',
+    'llama3.3',
+    'gemma2',
+    'gemma2:27b',
+    'mixtral:8x7b',
+    'mistral',
+    'mistral-small',
+    'phi4:14b',
+    'hf.co/unsloth/phi-4-GGUF',
+    'qwen2',
+    'qwen2.5:32b',
+    'qwen2.5-coder',
+    'deepseek-r1',
+    'deepseek-r1:32b'
+];
+addOptionsToSelect('customModel', options);
