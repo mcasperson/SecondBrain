41cc38a5dbe204bedbcb16eb7a7af4cc7f92e2ab
Matthew Casperson <matthewcasperson@gmail.com>
2025-03-24 14:33:05 +1000

    Try gemma 3 from unsloth

--- scripts/OrganizationMultiGeneratePdf.ps1
+++ scripts/OrganizationMultiGeneratePdf.ps1
@@ -44,7 +44,8 @@ $toolModel = "llama3.1"
 #$model = "llama3.3"
 #$model = "gemma2:27b"
 #$model = "mistral-small"
-$model = "gemma3:27b"
+#$model = "gemma3:27b"
+$model = "hf.co/unsloth/gemma-3-27b-it-GGUF:Q4_K_M"
 #$model = "qwen2.5:32b"
 
 # 128K tokens can be just a bit too much when using a 70B model
