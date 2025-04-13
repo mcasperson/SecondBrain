6f33ebaef05cd08e368cdc2bdd4dfa45aa12c18c
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-13 18:18:51 +1000

    Added a test of phi4-mini

--- core/src/test/java/secondbrain/domain/prompt/PromptBuilderPhiTest.java
+++ core/src/test/java/secondbrain/domain/prompt/PromptBuilderPhiTest.java
@@ -14,6 +14,7 @@ public class PromptBuilderPhiTest {
 
         assertTrue("hf.co/unsloth/phi-4-GGUF".matches(regex));
         assertTrue("phi3:14b".matches(regex));
+        assertTrue("phi4-mini".matches(regex));
         assertFalse("hf.co/unsloth/gemma2".matches(regex));
         assertFalse("mistral".matches(regex));
     }
