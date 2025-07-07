1866ae659df2788c3f5316573eee0e6e8cfd7301
Matthew Casperson <matthewcasperson@gmail.com>
2025-07-08 06:35:07 +1000

    Get more user details

--- tools/src/main/java/secondbrain/infrastructure/zendesk/api/ZenDeskUserItemResponse.java
+++ tools/src/main/java/secondbrain/infrastructure/zendesk/api/ZenDeskUserItemResponse.java
@@ -3,5 +3,5 @@ package secondbrain.infrastructure.zendesk.api;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ZenDeskUserItemResponse(String name, String id) {
+public record ZenDeskUserItemResponse(String name, String id, String email, Long organization_id) {
 }
