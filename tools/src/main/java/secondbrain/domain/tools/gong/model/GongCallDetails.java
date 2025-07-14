8070c8ae8f5b539a4baed2afdf1074db3ec1a44d
Matthew Casperson <matthewcasperson@gmail.com>
2025-07-14 12:06:54 +1000

    Fixed speakerid

--- tools/src/main/java/secondbrain/domain/tools/gong/model/GongCallDetails.java
+++ tools/src/main/java/secondbrain/domain/tools/gong/model/GongCallDetails.java
@@ -14,18 +14,18 @@ import java.util.List;
  */
 public record GongCallDetails(String id, String url, List<GongCallExtensiveParty> parties) {
     @Nullable
-    public GongCallExtensiveParty getPartyFromId(final String partyId) {
+    public GongCallExtensiveParty getPartyFromId(final String speakerId) {
         if (parties == null || parties.isEmpty()) {
             return null;
         }
         return parties.stream()
-                .filter(party -> party.id().equals(partyId))
+                .filter(party -> party.speakerId().equals(speakerId))
                 .findFirst()
                 .orElse(null);
     }
 
     public String getPartyNameFromId(final String partyId) {
         final GongCallExtensiveParty party = getPartyFromId(partyId);
-        return party != null ? party.name() : "Unknown";
+        return party != null ? party.name() : "Unknown Speaker";
     }
 }
