c85de8e151fb295355300a2a482dfef78b62c1b1
Matthew Casperson <matthewcasperson@gmail.com>
2025-07-14 15:18:07 +1000

    Fixed tests

--- core/src/test/java/secondbrain/domain/context/RagMultiDocumentContextTest.java
+++ core/src/test/java/secondbrain/domain/context/RagMultiDocumentContextTest.java
@@ -57,8 +57,8 @@ class RagMultiDocumentContextTest {
                 similarityCalculator,
                 sentenceVectorizer
         );
-        assertTrue(annotatedDocument.annotatedContent().contains("[1]: This is a test document"));
-        assertTrue(annotatedDocument.annotatedContent().contains("[2]: It contains multiple sentences"));
+        assertTrue(annotatedDocument.annotations().contains("[1]: This is a test document"));
+        assertTrue(annotatedDocument.annotations().contains("[2]: It contains multiple sentences"));
         assertEquals(1, annotatedDocument.annotationCoverage());
     }
 
