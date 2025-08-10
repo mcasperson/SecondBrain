a5a67abc8129a86ce9ee7434f42ff81ddf27ee39
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-11 06:36:40 +1000

    Use simple logging

--- core/src/main/java/secondbrain/domain/logger/Loggers.java
+++ core/src/main/java/secondbrain/domain/logger/Loggers.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.inject.spi.InjectionPoint;
 
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @ApplicationScoped
 public class Loggers {
@@ -15,8 +16,13 @@ public class Loggers {
         final Logger logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
                 .getSimpleName());
 
+        final SimpleFormatter formatter = new SimpleFormatter();
+
         Try.of(() -> new FileHandler("SecondBrain.log", true))
-                .onSuccess(logger::addHandler)
+                .onSuccess(handler -> {
+                    handler.setFormatter(formatter);
+                    logger.addHandler(handler);
+                })
                 .onFailure(throwable -> logger.warning("Failed to create logger file handler: " + throwable.getMessage()));
 
         return logger;
