69a3f58c93856454b1e16b7327ddb496628c2578
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-11 07:08:55 +1000

    Fixed shared file handler

--- core/src/main/java/secondbrain/domain/logger/Loggers.java
+++ core/src/main/java/secondbrain/domain/logger/Loggers.java
@@ -1,29 +1,41 @@
 package secondbrain.domain.logger;
 
 import io.vavr.control.Try;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
+import org.jspecify.annotations.Nullable;
 
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 @ApplicationScoped
 public class Loggers {
+
+    @Nullable
+    private FileHandler fileHandler;
+
+    @PostConstruct
+    private void init() {
+        final SimpleFormatter formatter = new SimpleFormatter();
+        this.fileHandler = Try.of(() -> new FileHandler("SecondBrain.log", true))
+                .onSuccess(handler -> handler.setFormatter(formatter))
+                .getOrNull();
+    }
+
     @Produces
     public Logger getLogger(final InjectionPoint injectionPoint) {
         final Logger logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
                 .getSimpleName());
 
-        final SimpleFormatter formatter = new SimpleFormatter();
+        logger.setLevel(Level.INFO);
 
-        Try.of(() -> new FileHandler("SecondBrain.log", true))
-                .onSuccess(handler -> {
-                    handler.setFormatter(formatter);
-                    logger.addHandler(handler);
-                })
-                .onFailure(throwable -> logger.warning("Failed to create logger file handler: " + throwable.getMessage()));
+        if (fileHandler != null) {
+            logger.addHandler(fileHandler);
+        }
 
         return logger;
     }
