7b2842540b4902cb4457f1008933fd10166acacb
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-11 06:33:58 +1000

    Log to a file

--- core/src/main/java/secondbrain/domain/logger/Loggers.java
+++ core/src/main/java/secondbrain/domain/logger/Loggers.java
@@ -1,16 +1,24 @@
 package secondbrain.domain.logger;
 
+import io.vavr.control.Try;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
 
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 @ApplicationScoped
 public class Loggers {
     @Produces
     public Logger getLogger(final InjectionPoint injectionPoint) {
-        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
+        final Logger logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
                 .getSimpleName());
+
+        Try.of(() -> new FileHandler("SecondBrain.log", true))
+                .onSuccess(logger::addHandler)
+                .onFailure(throwable -> logger.warning("Failed to create logger file handler: " + throwable.getMessage()));
+
+        return logger;
     }
 }
