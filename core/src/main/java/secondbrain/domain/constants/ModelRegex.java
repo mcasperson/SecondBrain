6a295447cc3ad41393a17e63c1daf3bc7cb871a0
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-11 11:02:20 +1000

    Allow reasoning models

--- core/src/main/java/secondbrain/domain/constants/ModelRegex.java
+++ core/src/main/java/secondbrain/domain/constants/ModelRegex.java
@@ -3,5 +3,5 @@ package secondbrain.domain.constants;
 public class ModelRegex {
     public static final String QWEN_REGEX = "^(hf.co/unsloth/)?(qwq|(q|Q)wen\\d(\\.\\d)?).*$";
     public static final String DEEPSEEK_REGEX = "^deepseek-r1.*$";
-    public static final String PHI4_REGEX = "^(Phi-4)|(phi4)$";
+    public static final String PHI4_REGEX = "^(Phi-4(-reasoning)?|(phi4)$";
 }
