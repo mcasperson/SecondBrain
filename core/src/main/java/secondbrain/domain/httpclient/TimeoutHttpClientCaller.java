538ccd1a573ea20fad7814fedd53057248ca906c
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-20 18:35:01 +1000

    Added some comments

--- core/src/main/java/secondbrain/domain/httpclient/TimeoutHttpClientCaller.java
+++ core/src/main/java/secondbrain/domain/httpclient/TimeoutHttpClientCaller.java
@@ -2,6 +2,10 @@ package secondbrain.domain.httpclient;
 
 import secondbrain.domain.timeout.TimeoutFunctionCallback;
 
+/**
+ * Defines a service that builds a client, generates the response, parses the response, builds an exception, and offers reties with a timeout.
+ * This service will also take care of closing any resources.
+ */
 public interface TimeoutHttpClientCaller {
     <T> T call(ClientBuilder builder, ClientCallback callback, ResponseCallback responseCallback, ExceptionBuilder exceptionBuilder,
                TimeoutFunctionCallback<T> timeoutCallback, long timeoutSeconds, long retryDelaySeconds, int maxRetries);
