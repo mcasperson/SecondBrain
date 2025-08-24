d3b7c8bdee022c35bafdc5435abcbf635f64dfea
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-24 15:03:45 +1000

    Deal with null values

--- tools/src/main/java/secondbrain/infrastructure/github/api/GitHubCommitAndDiff.java
+++ tools/src/main/java/secondbrain/infrastructure/github/api/GitHubCommitAndDiff.java
@@ -2,9 +2,19 @@ package secondbrain.infrastructure.github.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Objects;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GitHubCommitAndDiff(GitHubCommitResponse commit, String diff) {
     public String getMessageAndDiff() {
-        return "Commit message:\n" + commit.commit().message() + "\nDiff:\n" + diff;
+        if (commit != null && commit.commit() != null && commit.commit().message() != null) {
+            return "Commit message:\n" + commit.commit().message() + "\nDiff:\n" + getDiff();
+        }
+
+        return getDiff();
+    }
+
+    public String getDiff() {
+        return Objects.requireNonNullElse(diff, "");
     }
 }
