e71a2c0b6a6412e8bf1fa348d43b2d4a720a390a
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-26 19:03:41 +1000

    Fixed test

--- core/src/test/java/secondbrain/domain/vector/JdlSentenceVectorizerTest.java
+++ core/src/test/java/secondbrain/domain/vector/JdlSentenceVectorizerTest.java
@@ -1,15 +1,34 @@
 package secondbrain.domain.vector;
 
+import io.smallrye.config.inject.ConfigExtension;
+import jakarta.inject.Inject;
+import org.jboss.weld.junit5.auto.AddBeanClasses;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
 import org.junit.jupiter.api.Test;
 import secondbrain.domain.context.JdlSentenceVectorizer;
 import secondbrain.domain.context.RagStringContext;
+import secondbrain.domain.exceptionhandling.LoggingExceptionHandler;
+import secondbrain.domain.json.JsonDeserializerJackson;
+import secondbrain.domain.logger.Loggers;
+import secondbrain.domain.persist.H2LocalStorage;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@EnableAutoWeld
+@AddExtensions(ConfigExtension.class)
+@AddBeanClasses(JdlSentenceVectorizer.class)
+@AddBeanClasses(H2LocalStorage.class)
+@AddBeanClasses(Loggers.class)
+@AddBeanClasses(JsonDeserializerJackson.class)
+@AddBeanClasses(LoggingExceptionHandler.class)
 public class JdlSentenceVectorizerTest {
+
+    @Inject
+    private JdlSentenceVectorizer jdlSentenceVectorizer;
+
     @Test
     public void testVectorize() {
-        final JdlSentenceVectorizer jdlSentenceVectorizer = new JdlSentenceVectorizer();
         final String text = "This is a test sentence.";
         final RagStringContext vector = jdlSentenceVectorizer.vectorize(text);
         assertNotNull(vector.vector());
