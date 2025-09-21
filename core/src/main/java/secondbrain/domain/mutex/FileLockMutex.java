a059c892329b8556565298b923e5fb35f31092b6
Matthew Casperson <matthewcasperson@gmail.com>
2025-09-21 14:28:09 +1000

    Lock at the JVM level first before falling through to a file lock.

--- core/src/main/java/secondbrain/domain/mutex/FileLockMutex.java
+++ core/src/main/java/secondbrain/domain/mutex/FileLockMutex.java
@@ -1,5 +1,6 @@
 package secondbrain.domain.mutex;
 
+import io.vavr.API;
 import io.vavr.control.Try;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -7,17 +8,42 @@ import secondbrain.domain.exceptions.LockFail;
 
 import java.io.RandomAccessFile;
 import java.nio.channels.FileLock;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
 
 @ApplicationScoped
 public class FileLockMutex implements Mutex {
     private static final long SLEEP = 10000;
+    private static final Map<String, ReentrantLock> LOCKS = new ConcurrentHashMap<>();
 
     @Inject
     private Logger log;
 
     @Override
     public <T> T acquire(final long timeout, final String lockName, final MutexCallback<T> callback) {
+        final ReentrantLock localLock = LOCKS.computeIfAbsent(lockName, k -> new ReentrantLock());
+
+        // We lock at the JVM level first to avoid multiple threads in the same JVM from trying to acquire the file lock simultaneously.
+        return Try.of(() -> localLock.tryLock(timeout, TimeUnit.MILLISECONDS))
+                // We only proceed if we acquired the local lock.
+                .filter(acquired -> acquired)
+                // If we fail to acquire the local lock, we throw a LockFail exception.
+                .mapFailure(API.Case(API.$(), ex -> new LockFail("Failed to obtain file lock within the specified timeout", ex)))
+                // If we acquired the local lock, we proceed to establish the file lock.
+                .map(a -> establishFileLock(timeout, lockName, callback))
+                // Ensure we always release the local lock if we acquired it.
+                .andFinally(() -> {
+                    if (localLock.isHeldByCurrentThread()) {
+                        localLock.unlock();
+                    }
+                })
+                .get();
+    }
+
+    public <T> T establishFileLock(final long timeout, final String lockName, final MutexCallback<T> callback) {
         return Try.withResources(() -> new RandomAccessFile(lockName, "rw").getChannel())
                 .of(channel -> Try.withResources(() -> channel.tryLock(0, 0, false))
                         .of(lock -> callIfNotNull(lock, callback))
