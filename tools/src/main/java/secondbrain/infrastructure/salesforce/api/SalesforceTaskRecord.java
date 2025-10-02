9b931bae3d1052d9f80bf518b8683b4da519bce7
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-02 15:33:34 +1000

    Have now got salesforce emails being returned correctly

--- tools/src/main/java/secondbrain/infrastructure/salesforce/api/SalesforceTaskRecord.java
+++ tools/src/main/java/secondbrain/infrastructure/salesforce/api/SalesforceTaskRecord.java
@@ -1,9 +1,11 @@
 package secondbrain.infrastructure.salesforce.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SalesforceTaskRecord(String id, String description, String type, String subject) {
+public record SalesforceTaskRecord(@JsonProperty("Id") String id, @JsonProperty("Description") String description,
+                                   @JsonProperty("Type") String type, @JsonProperty("Subject") String subject) {
     public String getEmailText() {
         return subject + "\n" + description;
     }
