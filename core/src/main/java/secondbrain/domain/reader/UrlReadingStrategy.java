dc70c25a1caa792a956df460fff44e69a6f3237b
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-19 10:14:45 +1000

    Small tidy up

--- core/src/main/java/secondbrain/domain/reader/UrlReadingStrategy.java
+++ core/src/main/java/secondbrain/domain/reader/UrlReadingStrategy.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import org.apache.commons.lang3.ArrayUtils;
 import secondbrain.domain.concurrency.SemaphoreLender;
 import secondbrain.domain.constants.Constants;
 import secondbrain.domain.exceptions.InvalidResponse;
@@ -12,6 +13,8 @@ import secondbrain.domain.response.ResponseValidation;
 
 @ApplicationScoped
 public class UrlReadingStrategy implements FileReadingStrategy {
+    private static final int REDIRECT_LIMIT = 5;
+    private static final int[] REDIRECT_STATUSES = {301, 302};
     private static final SemaphoreLender SEMAPHORE_LENDER = new SemaphoreLender(Constants.DEFAULT_SEMAPHORE_COUNT);
 
     @Inject
@@ -24,13 +27,13 @@ public class UrlReadingStrategy implements FileReadingStrategy {
     }
 
     private String downloadDocumentLoop(final Client client, final String url, int retry) {
-        if (retry > 5) {
+        if (retry > REDIRECT_LIMIT) {
             throw new InvalidResponse("Failed to get document from " + url);
         }
 
         return Try.withResources(() -> client.target(url).request().get())
                 .of(response -> {
-                    if (response.getStatus() == 302 || response.getStatus() == 301) {
+                    if (ArrayUtils.contains(REDIRECT_STATUSES, response.getStatus())) {
                         return downloadDocumentLoop(client, response.getHeaderString("Location"), retry + 1);
                     } else {
                         return responseValidation.validate(response, url).readEntity(String.class);
