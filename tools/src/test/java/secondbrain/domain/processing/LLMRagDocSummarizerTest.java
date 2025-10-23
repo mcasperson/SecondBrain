d8b2357e2b9550f7f5e994b0bed6ba1978ab6ea1
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-24 06:58:13 +1000

    Update test configuration to set encryption password for tests

--- tools/src/test/java/secondbrain/domain/processing/LLMRagDocSummarizerTest.java
+++ tools/src/test/java/secondbrain/domain/processing/LLMRagDocSummarizerTest.java
@@ -1,9 +1,13 @@
 package secondbrain.domain.processing;
 
+import io.smallrye.config.PropertiesConfigSource;
+import io.smallrye.config.SmallRyeConfigBuilder;
 import io.smallrye.config.inject.ConfigExtension;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
@@ -36,6 +40,29 @@ class LLMRagDocSummarizerTest {
     private LocalConfigSummarizer config;
 
     private Map<String, String> environmentSettings;
+
+    @BeforeEach
+    void updateConfig() {
+        final var configSource = new PropertiesConfigSource(
+                Map.of(
+                        "sb.encryption.password", "1234567890"
+                ),
+                "TestConfig",
+                Integer.MAX_VALUE
+        );
+        final Config newConfig = new SmallRyeConfigBuilder()
+                .withSources(configSource)
+                .build();
+
+        final var configProviderResolver = ConfigProviderResolver.instance();
+        final var oldConfig = configProviderResolver.getConfig();
+
+        configProviderResolver.releaseConfig(oldConfig);
+        configProviderResolver.registerConfig(
+                newConfig,
+                Thread.currentThread().getContextClassLoader()
+        );
+    }
 
     @Produces
     @Preferred
