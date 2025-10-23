5b663ab36ad2b86f1be1fc0fd9887d2ec4454644
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-24 06:43:20 +1000

    Update test configuration to use a custom encryption password

--- core/src/test/java/secondbrain/domain/vector/JdlSentenceVectorizerTest.java
+++ core/src/test/java/secondbrain/domain/vector/JdlSentenceVectorizerTest.java
@@ -1,10 +1,15 @@
 package secondbrain.domain.vector;
 
+import io.smallrye.config.PropertiesConfigSource;
+import io.smallrye.config.SmallRyeConfigBuilder;
 import io.smallrye.config.inject.ConfigExtension;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import secondbrain.domain.context.JdlSentenceVectorizer;
 import secondbrain.domain.context.RagStringContext;
@@ -14,6 +19,8 @@ import secondbrain.domain.json.JsonDeserializerJackson;
 import secondbrain.domain.logger.Loggers;
 import secondbrain.domain.persist.H2LocalStorage;
 import secondbrain.domain.persist.LocalStorageProducer;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -30,6 +37,29 @@ public class JdlSentenceVectorizerTest {
 
     @Inject
     private JdlSentenceVectorizer jdlSentenceVectorizer;
+    
+    @BeforeEach
+    void updateConfig() {
+        final var configSource = new PropertiesConfigSource(
+                Map.of(
+                        "sb.encryption.password", "1234567890"
+                ),
+                "TestConfig",
+                Integer.MAX_VALUE
+        );
+        final Config newConfig = new SmallRyeConfigBuilder()
+                .withSources(configSource)
+                .build();
+
+        final var configProviderResolver = ConfigProviderResolver.instance();
+        final var oldConfig = configProviderResolver.getConfig();
+
+        configProviderResolver.releaseConfig(oldConfig);
+        configProviderResolver.registerConfig(
+                newConfig,
+                Thread.currentThread().getContextClassLoader()
+        );
+    }
 
     @Test
     public void testVectorize() {
