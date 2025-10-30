d96906c8f986e7f88d2e764ddfce532553b92a8f
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-31 06:56:52 +1000

    Inject logger level configuration and update logging level initialization

--- core/src/main/java/secondbrain/domain/logger/Loggers.java
+++ core/src/main/java/secondbrain/domain/logger/Loggers.java
@@ -5,8 +5,11 @@ import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jspecify.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +20,10 @@ public class Loggers {
 
     @Nullable
     private FileHandler fileHandler;
+
+    @Inject
+    @ConfigProperty(name = "sb.logger.level", defaultValue = "INFO")
+    private Optional<String> loggerLevel;
 
     @PostConstruct
     private void init() {
@@ -32,7 +39,18 @@ public class Loggers {
         final Logger logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
                 .getSimpleName());
 
-        logger.setLevel(Level.INFO);
+        final String level = loggerLevel.orElse("INFO");
+        if (level.equalsIgnoreCase("FINE")) {
+            logger.setLevel(Level.FINE);
+        } else if (level.equalsIgnoreCase("INFO")) {
+            logger.setLevel(Level.INFO);
+        } else if (level.equalsIgnoreCase("WARNING")) {
+            logger.setLevel(Level.WARNING);
+        } else if (level.equalsIgnoreCase("ERROR")) {
+            logger.setLevel(Level.SEVERE);
+        } else {
+            logger.setLevel(Level.INFO);
+        }
 
         if (fileHandler != null) {
             logger.addHandler(fileHandler);
