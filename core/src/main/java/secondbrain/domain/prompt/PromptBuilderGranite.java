7b437a9bf64b638c086c0a23be7ca3cca871c889
Matthew Casperson <matthew.casperson@octopus.com>
2025-12-27 16:04:58 +1000

    Add PromptBuilderGranite class for enhanced prompt building functionality

---
+++ core/src/main/java/secondbrain/domain/prompt/PromptBuilderGranite.java
@@ -0,0 +1,72 @@
+package secondbrain.domain.prompt;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jspecify.annotations.Nullable;
+import secondbrain.domain.constants.ModelRegex;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class PromptBuilderGranite implements PromptBuilder {
+    @Inject
+    @ConfigProperty(name = "sb.qwen.thinking", defaultValue = "false")
+    private Optional<String> thinking;
+
+    @Override
+    public String modelRegex() {
+        return ModelRegex.QWEN_REGEX;
+    }
+
+    @Override
+    public String buildContextPrompt(final String title, final String prompt) {
+        if (StringUtils.isBlank(prompt)) {
+            return "";
+        }
+
+        if (StringUtils.isBlank(title)) {
+            return "---------------------\n"
+                    + prompt + "\n"
+                    + "---------------------";
+        }
+
+        return "---------------------\n"
+                + title + ":\n"
+                + prompt + "\n"
+                + "---------------------";
+    }
+
+    @Override
+    public String buildFinalPrompt(@Nullable final String instructions, final String context, final String prompt) {
+        if (StringUtils.isBlank(instructions)) {
+            return StringUtils.trim(context)
+                    + "\n<|im_end|>\n"
+                    + "\n<|im_start|>user\n"
+                    + prompt
+                    + "\n<|im_end|>"
+                    + "\n<|im_start|>assistant";
+        }
+
+        return "<|im_start|>system\n"
+                + StringUtils.trim(context)
+                + "\n"
+                + StringUtils.trim(instructions)
+                + "\n<|im_end|>\n"
+                + "\n<|im_start|>user\n"
+                + prompt
+                + "\n<|im_end|>"
+                + getThinking()
+                + "\n<|im_start|>assistant";
+    }
+
+    private String getThinking() {
+        return thinking != null && !Boolean.parseBoolean(thinking.orElse("false"))
+                ? """
+                <|im_start|>system
+                \\no_think
+                <|im_end|>""".stripIndent()
+                : "";
+    }
+}
