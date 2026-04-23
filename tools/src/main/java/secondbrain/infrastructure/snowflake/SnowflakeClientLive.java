65174ff1b44233bd4148d050e21a43568beb96bb
Matthew Casperson <matthewcasperson@gmail.com>
2026-04-23 13:09:34 +0800

    Implement connection management in SnowflakeClientLive and update license details retrieval method

--- tools/src/main/java/secondbrain/infrastructure/snowflake/SnowflakeClientLive.java
+++ tools/src/main/java/secondbrain/infrastructure/snowflake/SnowflakeClientLive.java
@@ -1,14 +1,17 @@
 package secondbrain.infrastructure.snowflake;
 
+import com.google.common.base.Preconditions;
 import io.vavr.control.Try;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.jspecify.annotations.Nullable;
 
 import java.io.StringReader;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.Base64;
@@ -16,19 +19,14 @@ import java.util.Properties;
 
 public class SnowflakeClientLive {
 
-    public PrivateKey getPrivateKeyFromPEM(String pemString) throws Exception {
-        PEMParser pemParser = new PEMParser(new StringReader(pemString));
-        Object object = pemParser.readObject();
+    @Nullable
+    private Connection connection;
 
-        // PEMKeyPair is used for PKCS#1 format
-        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-        if (object instanceof PEMKeyPair) {
-            return converter.getKeyPair((PEMKeyPair) object).getPrivate();
+    public void openConnection(final String username, final String pem, final String url) {
+        if (connection != null) {
+            return;
         }
-        throw new IllegalArgumentException("Not a valid PKCS#1 RSA private key");
-    }
 
-    public ResultSet getLicenseDetails(final String username, final String pem, final String url) {
         final PrivateKey privateKey = Try.of(() -> getPrivateKeyFromPEM(pem)).get();
 
         Properties properties = new Properties();
@@ -36,13 +34,38 @@ public class SnowflakeClientLive {
         properties.put("authenticator", "SNOWFLAKE_JWT");
         properties.put("privateKey", privateKey);
         properties.put("JDBC_QUERY_RESULT_FORMAT", "json");
+        connection = Try.of(() -> DriverManager.getConnection(url, properties)).get();
+    }
 
-        return Try.withResources(() -> DriverManager.getConnection(url, properties))
-                .of(connection -> {
-                    // Create and execute a query
-                    var statement = connection.createStatement();
-                    return statement.executeQuery("SELECT * from prod_model.integration.integration_account_usage_summary");
-                })
+    @SuppressWarnings("NullAway")
+    public void closeConnection() {
+        if (connection == null) {
+            return;
+        }
+
+        Try.run(() -> this.connection.close());
+        this.connection = null;
+    }
+
+    public PrivateKey getPrivateKeyFromPEM(String pemString) throws Exception {
+        PEMParser pemParser = new PEMParser(new StringReader(pemString));
+        Object object = pemParser.readObject();
+
+        // PEMKeyPair is used for PKCS#1 format
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+        if (object instanceof PEMKeyPair pemKeyPair) {
+            return converter.getKeyPair(pemKeyPair).getPrivate();
+        }
+        throw new IllegalArgumentException("Not a valid PKCS#1 RSA private key");
+    }
+
+    @SuppressWarnings("NullAway")
+    public ResultSet getLicenseDetails() {
+        Preconditions.checkArgument(connection != null, "Connection must be established before querying");
+
+        return Try.of(() -> connection.createStatement())
+                .mapTry(statement -> statement.executeQuery("SELECT * from prod_model.integration.integration_account_usage_summary"))
                 .get();
+
     }
 }
--- tools/src/test/java/secondbrain/infrastructure/snowflake/SnowflakeClientLiveTest.java
+++ tools/src/test/java/secondbrain/infrastructure/snowflake/SnowflakeClientLiveTest.java
@@ -21,11 +21,22 @@ class SnowflakeClientLiveTest {
     void testConnection() throws SQLException {
         final SnowflakeClientLive snowflakeClientLive = new SnowflakeClientLive();
         final String jwt = new String(Base64.getDecoder().decode(System.getenv("SB_SNOWFLAKE_JWT_BASE64")));
-        final ResultSet rs = snowflakeClientLive.getLicenseDetails(
+        snowflakeClientLive.openConnection(
                 System.getenv("SB_SNOWFLAKE_USER"),
                 jwt,
                 "jdbc:snowflake://" + System.getenv("SB_SNOWFLAKE_URL") + ".snowflakecomputing.com");
+        final ResultSet rs = snowflakeClientLive.getLicenseDetails();
         assertNotNull(rs);
+
+        final int columnCount = rs.getMetaData().getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.println(rs.getMetaData().getColumnName(i) + ": " + rs.getString(i));
+            }
+            System.out.println("---");
+        }
+
+        snowflakeClientLive.closeConnection();
     }
 }
 
