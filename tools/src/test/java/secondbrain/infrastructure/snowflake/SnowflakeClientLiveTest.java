8ae558c75a6697f36bc0aee2b93d183d51015489
Matthew Casperson <matthewcasperson@gmail.com>
2026-04-23 13:30:28 +0800

    Disable testConnection method in SnowflakeClientLiveTest to prevent execution during test runs

--- tools/src/test/java/secondbrain/infrastructure/snowflake/SnowflakeClientLiveTest.java
+++ tools/src/test/java/secondbrain/infrastructure/snowflake/SnowflakeClientLiveTest.java
@@ -1,5 +1,6 @@
 package secondbrain.infrastructure.snowflake;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Base64;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class SnowflakeClientLiveTest {
     @Test
+    @Disabled
     void testConnection() {
         final SnowflakeClientLive snowflakeClientLive = new SnowflakeClientLive();
         final String jwt = new String(Base64.getDecoder().decode(System.getenv("SB_SNOWFLAKE_JWT_BASE64")));
