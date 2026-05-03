dc4e8efa9b9df61e9a6d547eacd15bc902215239
Matthew Casperson <matthewcasperson@gmail.com>
2026-05-03 18:03:22 +1000

    Refactor Vector class to use primitive double array for reduced memory usage

--- core/src/main/java/secondbrain/domain/context/Vector.java
+++ core/src/main/java/secondbrain/domain/context/Vector.java
@@ -1,55 +1,66 @@
 package secondbrain.domain.context;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 /**
- * Represents a vector of double values.
+ * Represents a vector of double values backed by a primitive double array
+ * to minimize heap usage (avoids boxing each value as a Double object).
  *
- * @param value The list of double values
+ * @param value The primitive double array
  */
-public record Vector(List<Double> value) {
+public record Vector(double[] value) {
 
     public Vector(final Double... value) {
-        this(Arrays.asList(value));
+        this(Arrays.stream(value).mapToDouble(Double::doubleValue).toArray());
     }
 
-    public Vector(final double[] values) {
-        this(Arrays.stream(values).boxed().toList());
-    }
-
-    public Vector(final List<Double> value) {
+    public Vector(final double[] value) {
         this.value = Objects.requireNonNull(value);
     }
 
     public int dimension() {
-        return value.size();
+        return value.length;
     }
 
     public double get(final int index) {
-        if (index < 0 || index >= value.size()) {
+        if (index < 0 || index >= value.length) {
             throw new IndexOutOfBoundsException("Index: " + index + " is out of bound");
         }
-        return value.get(index);
+        return value[index];
     }
 
     public double dotProduct(final Vector other) {
         double dotProduct = 0.0;
 
-        for (int i = 0; i < value.size(); i++) {
-            dotProduct += this.get(i) * other.get(i);
+        for (int i = 0; i < value.length; i++) {
+            dotProduct += value[i] * other.value[i];
         }
         return dotProduct;
     }
 
     public double norm() {
-        return Math.sqrt(
-                value.stream()
-                        .mapToDouble(element -> Math.pow(element, 2))
-                        .sum()
-        );
+        double sum = 0.0;
+        for (final double v : value) {
+            sum += v * v;
+        }
+        return Math.sqrt(sum);
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector other)) return false;
+        return Arrays.equals(value, other.value);
+    }
 
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector[value=" + Arrays.toString(value) + "]";
+    }
 }
